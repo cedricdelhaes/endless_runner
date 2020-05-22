a268f21f8378a05d5eb6cdaf91ac7480edca24f2
cedric.delhaes <delhaes.c@gmail.com>
2020-05-22 15:08:11 +0200

    Add bar and heart png, upgrade spirit slider to bar slider

--- Endless Runner/Assets/Scripts/PlayerBehaviour.cs
+++ Endless Runner/Assets/Scripts/PlayerBehaviour.cs
@@ -6,9 +6,15 @@ using UnityEngine.UI;
 public class PlayerBehaviour : MonoBehaviour
 {
 
-    private bool spiritAngel;
+    /*Spirit slider*/
+    private bool _spiritAngel;
     public Slider spiritSlider;
+    public Image spiritFill;
     public float spiritIncrement;
+    public int spiritMaxValue;
+    public int spiritMinValue;
+    public Gradient spiritGradiant;
+    private float _sliderIncrementWidth;
 
     public bool invincible;
 
@@ -25,17 +31,48 @@ public class PlayerBehaviour : MonoBehaviour
     }
 
     private void Start() {
-        spiritAngel = true;
-        spiritSlider.value = 0;
-    }  
+
+        /*Init spirit bar data*/
+        _spiritAngel = true;
+        spiritSlider.minValue = spiritMinValue;
+        spiritSlider.maxValue = spiritMaxValue;
+        spiritSlider.value = (spiritMaxValue-Mathf.Abs(spiritMinValue))/2;
+
+        spiritFill.color = spiritGradiant.Evaluate(spiritSlider.normalizedValue);
+
+        _sliderIncrementWidth = spiritSlider.GetComponent<RectTransform>().rect.width / (spiritMaxValue - spiritMinValue);
+
+    }
+
+    private void spiritAutoUpdateValue()
+    {
+        if (_spiritAngel)
+            spiritSlider.value += spiritIncrement * Time.deltaTime;
+
+        else
+            spiritSlider.value -= spiritIncrement * Time.deltaTime;
+    }
 
     private void Update(){
 
         CheckSpiritValue();
-        if (spiritAngel)
-            spiritSlider.value += spiritIncrement;
+        InvokeRepeating("spiritAutoUpdateValue", 0.5f, 10f);
+
+        //Put correct color from gradiant color according to slider value
+        spiritFill.color = spiritGradiant.Evaluate(spiritSlider.normalizedValue);
+
+        /*Change slider behaviour to center the slider starting point*/
+        spiritSlider.fillRect.rotation = new Quaternion(0, 0, 0, 0);
+        spiritSlider.fillRect.pivot = new Vector2(spiritSlider.fillRect.localPosition.x, spiritSlider.fillRect.pivot.y);
+        if (spiritSlider.value > 0)
+             spiritSlider.fillRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, _sliderIncrementWidth * spiritSlider.value);
         else
-            spiritSlider.value -= spiritIncrement;
+        {
+            spiritSlider.fillRect.Rotate(0, 0, 180);
+            spiritSlider.fillRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, _sliderIncrementWidth * -spiritSlider.value);
+
+        }
+        spiritSlider.fillRect.localPosition = new Vector3(0, 0, 0);
     }
 
 
@@ -51,16 +88,16 @@ public class PlayerBehaviour : MonoBehaviour
     //If spiritValue get max value or min value lose life get back to zero
     private void CheckSpiritValue()
     {
-        if (spiritSlider.value == spiritSlider.maxValue || spiritSlider.value == spiritSlider.minValue)
+        if (spiritSlider.value >= spiritSlider.maxValue || spiritSlider.value <= spiritSlider.minValue)
         {
             if(!invincible)
                 life--;
-            spiritSlider.value = 0;
+            spiritSlider.value = (spiritMaxValue-Mathf.Abs(spiritMinValue))/2;
         }
     }
 
     public void SwitchSpirit(){
-        spiritAngel = !spiritAngel;
+        _spiritAngel = !_spiritAngel;
     }
 
 }
